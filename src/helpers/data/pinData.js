c0794a36d3d762eafe6a4bfad5d7b85e95c195ff
Chris Calhoun <[ctcalhoun2@gmail.com]>
2020-11-29 15:51:01 -0600

    can view public pins now.

--- src/helpers/data/pinData.js
+++ src/helpers/data/pinData.js
@@ -34,6 +34,14 @@ const updatePin = (object) => new Promise((resolve, reject) => {
 
 const deletePin = (fbKey) => axios.delete(`${baseUrl}/pin/${fbKey}.json`);
 
+const getPublicPins = () => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/pin.json?orderBy="private"&equalTo=false`).then((response) => {
+    const pinObjOfObjs = response.data;
+    console.warn(pinObjOfObjs);
+    resolve(Object.values(pinObjOfObjs));
+  }).catch((err) => reject(err));
+});
+
 // eslint-disable-next-line
 export default {
   getBoardPins,
@@ -42,4 +50,5 @@ export default {
   createPin,
   updatePin,
   deletePin,
+  getPublicPins,
 };
