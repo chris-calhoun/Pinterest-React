a23f770c716ca06b3916f115c97efc73e141c6f3
Chris Calhoun <[ctcalhoun2@gmail.com]>
2020-11-29 20:35:49 -0600

    removed pin details from public pins.

--- src/components/Card/PublicPinsCards.js
+++ src/components/Card/PublicPinsCards.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import AppModal from '../Modal/index';
 import PinToBoardForm from '../Forms/PinToBoardForm';
 
@@ -10,9 +10,9 @@ export default function PinsCard({ pin, onDelete }) {
       <div className='card-body'>
         <h5 className='card-title'>{pin.name}</h5>
         <p className='card-text'>{pin.description}</p>
-        <Link className='btn btn-primary' to={`/pin-details/${pin.firebaseKey}`}>
+        {/* <Link className='btn btn-primary' to={`/pin-details/${pin.firebaseKey}`}>
           Pin Details
-        </Link>
+        </Link> */}
         <AppModal title={'Add Pin to Board'} buttonLabel={'Add to Board'}><PinToBoardForm pinId={pin.firebaseKey} /></AppModal>
       </div>
     </div>
